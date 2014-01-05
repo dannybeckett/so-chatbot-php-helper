509b3a9ce94a54fe2e8dbb2defa2f738ccb23b2f
dannybeckett <daniel.beckett@gmail.com>
2014-01-05 09:39:28 +0000

    Update XmlToJson.php

--- XmlToJson.php
+++ XmlToJson.php
@@ -4,14 +4,12 @@
 	
 	class XmlToJson
 	{
-		public function Parse($url)
+		public function FromUrl($url)
 		{
-			$fileContents = file_get_contents($url);
-			$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
-			$fileContents = trim(str_replace('"', "'", $fileContents));
-			$simpleXml = simplexml_load_string($fileContents);
-			$json = json_encode($simpleXml);
-			return $json;
+			$xml = file_get_contents($url);
+			$xml = str_replace(array("\n", "\r", "\t"), '', $xml);
+			$xml = trim(str_replace('"', "'", $xml));
+			return json_encode(simplexml_load_string($xml));
 		}
 	}
 
