71a4713b85313d535f9186b299125817c310aad9
dannybeckett <daniel.beckett@gmail.com>
2014-01-05 09:39:49 +0000

    Update Weather.php

--- Weather.php
+++ Weather.php
@@ -8,6 +8,6 @@
 	}
 	
 	include 'XmlToJson.php';
-	print $_GET['callback'] . '(' . XmlToJson::Parse('http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=' . $airport . '&hoursBeforeNow=24&mostRecent=true') . ')';
+	print $_GET['callback'] . '(' . XmlToJson::FromUrl('http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=' . $airport . '&hoursBeforeNow=24&mostRecent=true') . ')';
 	
 ?>
