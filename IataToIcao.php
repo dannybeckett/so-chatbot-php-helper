85cd8fb1718d97085955635203a25273ffbcd12b
dannybeckett <daniel.beckett@gmail.com>
2014-01-06 12:59:53 +0000

    Update IataToIcao.php

--- IataToIcao.php
+++ IataToIcao.php
@@ -1,30 +1,28 @@
 <?php
 	
-	$airport = $_GET['a'];
-	$callback = $_GET['callback'];
-	
-	if(!$airport || !$callback || strlen($airport) !== 3)
+	class IataToIcao
 	{
-		die();
+		public function FromString($iata)
+		{
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, 'http://www.ourairports.com/search?q=' . $iata);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			
+			curl_exec($ch);
+			$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+			curl_close($ch);
+			
+			$icao = end(explode('/', rtrim($url, '/')));
+			
+			if(strlen($icao) !== 4)
+			{
+				return false;
+			}
+			
+			return $icao;
+		}
 	}
-	
-	$ch = curl_init();
-	
-	curl_setopt($ch, CURLOPT_URL, 'http://www.ourairports.com/search?q=' . $airport);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
-	curl_exec($ch);
-	$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-	curl_close($ch);
-	
-	$iata = end(explode('/', rtrim($url, '/')));
-	
-	if(strlen($iata) !== 4)
-	{
-		die('-1');
-	}
-	
-	echo $callback . '({"icao":"' . $iata . '"})';
 	
 ?>
