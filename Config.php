bf3bd5c49cb1bbb7b26bde92ef91ec4d365e873f
Danny Beckett <daniel.beckett@gmail.com>
2014-01-14 15:12:57 +0000

    Output if denied access

--- Config.php
+++ Config.php
@@ -22,7 +22,7 @@
 	if(!in_array(hash('sha512', $_SERVER['REMOTE_ADDR']), $whitelist))
 	{
 		header('HTTP/1.1 403 Forbidden');
-		exit;
+		die('You are not authorised to view this page!');
 	}
 	
 ?>
