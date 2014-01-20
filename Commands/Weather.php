4dfc9604fd0435990d85b0c5c32d2f8080a2b061
Danny Beckett <daniel.beckett@gmail.com>
2014-01-20 15:52:27 +0000

    Subdir for commands

--- Commands/Weather.php
+++ Commands/Weather.php
@@ -1,8 +1,8 @@
 <?php
 	
-	require_once('Config.php');
-	require_once('Classes/XMLToJSON.php');
-	require_once('Classes/Airport.php');
+	require_once('../Config.php');
+	require_once('../Classes/XMLToJSON.php');
+	require_once('../Classes/Airport.php');
 	
 	$airport = $_GET['a'];
 	$mode = $_GET['m'];
